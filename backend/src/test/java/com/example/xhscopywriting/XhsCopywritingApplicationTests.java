78485d24f7cab91f7d7176075dad615b7201304b
朱长清 <1964817536@qq.com>
2026-08-07 10:30:43 +0800

    Initial project setup

---
+++ backend/src/test/java/com/example/xhscopywriting/XhsCopywritingApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.xhscopywriting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XhsCopywritingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
